f96c2a655f968f1898cad0ea0095789c6173608b
Akash Manohar J <akash@akash.im>
2011-06-07 12:01:47 +0530

    added Author model. Switched to ActiveRecord-based session store
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Osrails::Application.config.session_store :cookie_store, key: '_osrails_session'
+# Osrails::Application.config.session_store :cookie_store, key: '_osrails_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Osrails::Application.config.session_store :active_record_store
+Osrails::Application.config.session_store :active_record_store
