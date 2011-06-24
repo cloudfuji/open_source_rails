4abcea01a2256cb8caddcf2283796808f8258290
Akash Manohar J <akash@akash.im>
2011-06-24 22:32:13 +0530

    added has_many bookmarks to user
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :cas_authenticatable,
          :recoverable, :rememberable, :trackable
 
+  has_many :bookmarks
+
   # Setup accessible (or protected) attributes for your model
   attr_accessible :username, :email, :password, :password_confirmation, :remember_me
 end
