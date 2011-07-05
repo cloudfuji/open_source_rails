84e2c3d606133909266c181169f9ac75a49ce0fb
Akash Manohar J <akash@akash.im>
2011-07-05 15:55:35 +0530

    cleanup and some work
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- config/initializers/sass_compass_workaround.rb
+++ config/initializers/sass_compass_workaround.rb
@@ -1,5 +1,6 @@
 Osrails::Application.config.sass.load_paths = [
   "#{Gem.loaded_specs['compass'].full_gem_path}/frameworks/compass/stylesheets",
-  "#{Gem.loaded_specs['compass'].full_gem_path}/frameworks/blueprint/stylesheets"
+  "#{Gem.loaded_specs['compass'].full_gem_path}/frameworks/blueprint/stylesheets",
+  "#{Gem.loaded_specs['fancy-buttons'].full_gem_path}/lib/stylesheets"
 ]
 
