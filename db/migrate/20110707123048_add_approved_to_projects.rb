09b6bd8053346a7e8aa412283941cbeccdf6d1a1
Akash Manohar J <akash@akash.im>
2011-07-07 20:02:55 +0530

    added true as default value for approve
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- db/migrate/20110707123048_add_approved_to_projects.rb
+++ db/migrate/20110707123048_add_approved_to_projects.rb
@@ -1,5 +1,5 @@
 class AddApprovedToProjects < ActiveRecord::Migration
   def change
-    add_column :projects, :approved, :boolean
+    add_column :projects, :approved, :boolean, :default=>false
   end
 end
