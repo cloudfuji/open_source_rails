410141647523ae2e5b5b3fb4a9c267abd4b122cd
Akash Manohar J <akash@akash.im>
2011-07-07 20:39:03 +0530

    cleaned up project unit test
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,23 +3,27 @@ require 'spec_helper'
 describe Project do
 
   describe "validates" do
-    it "presence of title" do
-      should validate_presence_of(:title)
+    context "title" do
+      it "presence of title" do
+        should validate_presence_of(:title)
+      end
+
+      it "length of title" do
+        should ensure_length_of(:title).is_at_least(1).is_at_most(100)
+      end
     end
 
-    it "length of title" do
-      should ensure_length_of(:title).is_at_least(1).is_at_most(100)
+    context do "short description" do
+      it "presense" do
+        should validate_presence_of(:short_desc)
+      end
+
+      it "length" do
+        should ensure_length_of(:short_desc).is_at_least(10)
+      end
     end
 
-    it "presense of short description" do
-      should validate_presence_of(:short_desc)
-    end
-
-    it "length of short description" do
-      should ensure_length_of(:short_desc).is_at_least(10)
-    end
-
-    describe "source_url" do
+    context "source_url" do
       it "presence" do
         should validate_presence_of(:source_url)
       end
