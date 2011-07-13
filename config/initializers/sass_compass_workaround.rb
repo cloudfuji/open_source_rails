da4ab1a2c24151d66b99102eae002e69bc1eca4a
Akash Manohar J <akash@akash.im>
2011-07-14 03:56:57 +0530

    removed previously added compass stuff
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- config/initializers/sass_compass_workaround.rb
+++
@@ -1,6 +0,0 @@
-Osrails::Application.config.sass.load_paths = [
-  "#{Gem.loaded_specs['compass'].full_gem_path}/frameworks/compass/stylesheets",
-  "#{Gem.loaded_specs['compass'].full_gem_path}/frameworks/blueprint/stylesheets",
-  "#{Gem.loaded_specs['fancy-buttons'].full_gem_path}/lib/stylesheets"
-]
-
