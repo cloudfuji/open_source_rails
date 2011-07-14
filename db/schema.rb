4beaa79548c34f26deeb70c925e94c13ead56e2a
Akash Manohar J <akash@akash.im>
2011-07-15 01:03:25 +0530

    added thumbnail to Project
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110714035724) do
+ActiveRecord::Schema.define(:version => 20110714193234) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.integer  "resource_id",   :null => false
@@ -70,7 +70,11 @@ ActiveRecord::Schema.define(:version => 20110714035724) do
     t.string   "source_url"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "approved",     :default => false
+    t.boolean  "approved",               :default => false
+    t.string   "thumbnail_file_name"
+    t.string   "thumbnail_content_type"
+    t.integer  "thumbnail_file_size"
+    t.datetime "thumbnail_updated_at"
   end
 
   create_table "screenshots", :force => true do |t|
