73edd2d93673cba0ae207fa48a3bc904f542fa0a
Akash Manohar J <akash@akash.im>
2011-07-18 22:25:04 +0530

    some cleanup in the JS
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- app/assets/javascripts/screenshot_handler.js
+++ app/assets/javascripts/screenshot_handler.js
@@ -3,24 +3,22 @@ $(document).ready(function(){
     var file = e.target.files[0];
     var objectURL=null;
     console.log(file);
-    
     console.log(getObjectURL(file));
 
   });  
 });
 
-function getObjectURL(fileObj) {
-  console.log("called");
-    if((typeof window.URL!=="undefined") && (typeof window.URL.createObjectURL=="function"))
-    {
-      //Firefox 3.6+ and Opera
-      console.log("FF", fileObj);
-      return window.URL.createObjectURL(file);
-    }
-    else if((typeof window.webkitURL != "undefined") && (typeof window.webkitURL.createObjectURL=="function"))  //Chrome 10+
-    {
-      console.log("Chrome",fileObj);
-      return window.webkitURL.createObjectURL(fileObj);
-    }
-    return null;
+function getObjectURL(obj) {
+  if((typeof window.URL!=="undefined") && (typeof window.URL.createObjectURL=="function"))
+  {
+    //Firefox 3.6+ and Opera
+    console.log("FF", obj);
+    return window.URL.createObjectURL(obj);
+  }
+  else if((typeof window.webkitURL != "undefined") && (typeof window.webkitURL.createObjectURL=="function"))  //Chrome 10+
+  {
+    console.log("Chrome", obj);
+    return window.webkitURL.createObjectURL(obj);
+  }
+  return null;
 }
