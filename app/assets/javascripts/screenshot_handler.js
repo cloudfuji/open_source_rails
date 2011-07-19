eb8b1bbffa37cdb7d33b5eec18fc82ee0f5a4219
Akash Manohar J <akash@akash.im>
2011-07-20 02:19:23 +0530

    now auto-fills author name and link
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- app/assets/javascripts/screenshot_handler.js
+++ app/assets/javascripts/screenshot_handler.js
@@ -10,19 +10,50 @@ $(document).ready(function(){
       // use jQmodal and add the ^ to that div, crop and display only cropped version 
     }
   });
+
+  $('.source_url').change(function(e) {
+    var pattern = /^(http(s)?:\/\/github\.com\/).+(\/).+(\.git)$/
+       ,repo_url = $(this).val();
+    
+    if(pattern.test(repo_url))
+    {
+      var repoString = repo_url.replace(/^http(s)?:\/\/github\.com\//i, "").replace(/\.git$/i, "");
+      console.log(repoString);
+      setRepoInfo(repoString);
+    }
+  });
+
+  function setRepoInfo(repoString) {
+    $.get('/projects/github_info/'+repoString+'.json'
+          ,data = {}
+          ,success = function(data, textStatus, xhr) {
+            if(data['repository']!=undefined)
+            {
+              var repo = data['repository']
+                 ,author = repo['owner']
+                 ,authorURL = "https://github/"+author;
+              $('.app_author').val(author);
+              $('.app_author_url').val(authorURL);
+              $('.app_author_link').html("<a href='"+authorURL+"'>"+author+"</a>");
+            }
+          }
+    );
+  }
+
+  function getObjectURL(obj) {
+    if((typeof window.URL!=="undefined") && (typeof window.URL.createObjectURL=="function"))
+    {
+      //Firefox 3.6+ and Opera
+      console.log("FF", obj);
+      return window.URL.createObjectURL(obj);
+    }
+    else if((typeof window.webkitURL != "undefined") && (typeof window.webkitURL.createObjectURL=="function"))  //Chrome 10+
+    {
+      console.log("Chrome", obj);
+      return window.webkitURL.createObjectURL(obj);
+    }
+    return null;
+  }
+
 });
 
-function getObjectURL(obj) {
-  if((typeof window.URL!=="undefined") && (typeof window.URL.createObjectURL=="function"))
-  {
-    //Firefox 3.6+ and Opera
-    console.log("FF", obj);
-    return window.URL.createObjectURL(obj);
-  }
-  else if((typeof window.webkitURL != "undefined") && (typeof window.webkitURL.createObjectURL=="function"))  //Chrome 10+
-  {
-    console.log("Chrome", obj);
-    return window.webkitURL.createObjectURL(obj);
-  }
-  return null;
-}
