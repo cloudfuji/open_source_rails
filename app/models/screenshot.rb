8c3ebd78a5ece097c86ff69eb96b42e57a02d0c6
Akash Manohar J <akash@akash.im>
2011-07-20 05:05:36 +0530

    Conquered cropped lands. ah, damn thing just works
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -5,6 +5,20 @@ class Screenshot < ActiveRecord::Base
   has_attached_file :image,
                     :styles=>{:large => "920x400"},
                     :url => "/store/:attachment/:id/:style/:basename.:extension",  
-                    :path => ":rails_root/permanent/store/:attachment/:id/:style/:basename.:extension"
+                    :path => ":rails_root/permanent/store/:attachment/:id/:style/:basename.:extension",
+                    :processors => [:cropper] 
+
   attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
+
+  after_save :reprocess_image, :if => :cropping?
+
+  def cropping?
+    !crop_x.blank? && !crop_y.blank?
+  end
+
+  private
+  
+  def reprocess_image
+    image.reprocess!
+  end
 end
