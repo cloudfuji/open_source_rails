7a7e41cb64098ab80a76d898ca6fd96b65892075
Akash Manohar J <akash@akash.im>
2011-07-20 05:34:59 +0530

    now rejecting screenshots if they are empty fields
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- app/models/project.rb
+++ app/models/project.rb
@@ -28,7 +28,8 @@ class Project < ActiveRecord::Base
             :format => { :with =>/\A(https:\/\/).+\.git\Z/i }
 
   accepts_nested_attributes_for :author, :allow_destroy=>true
-  accepts_nested_attributes_for :screenshots, :allow_destroy=>true
+  accepts_nested_attributes_for :screenshots, :allow_destroy=>true,
+                                :reject_if => proc { |attrs| attrs['image'].blank? }
 
   # approved is a boolean field and approved? is made available by rails
   # returns true if the project is not approved
