27c3206eac4ecfd567421885240cbf2cd45f41b5
Akash Manohar J <akash@akash.im>
2011-07-25 22:31:01 +0530

    completed ho projects#index; no more static stuff
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,7 +3,9 @@ class ProjectsController < ApplicationController
   before_filter :authenticate_user!, :only=>[:new, :create]
 
   def index
-    @featured_project = FeaturedProject.includes(:project=>[:project_category]).first.project
+    @featured_project = FeaturedProject.includes(:project=>[:project_category]).
+                                        first.project
+
     @projects = Project.includes(:project_category).
                         where("id != ? AND approved = ?", @featured_project, true)
   end
@@ -11,11 +13,15 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
     @project.build_author
-    5.times {@project.screenshots.build}
+    5.times {
+      @project.screenshots.build
+    }
   end
 
   def show
-    @project = Project.includes(:project_category, :screenshots, :author).find(params[:id])
+    @project = Project.includes(:project_category, :screenshots, :author).
+                       find(params[:id])
+
     unless @project.approved?
       redirect_to root_path, :notice=>"That project doesn't exist"
     end
@@ -23,6 +29,7 @@ class ProjectsController < ApplicationController
 
   def create
     @project = Project.new(params[:project])
+    
     @project.user_id = current_user.id
     if @project.save
       redirect_to @project
@@ -33,6 +40,7 @@ class ProjectsController < ApplicationController
 
   def github_info
     base_url = 'https://github.com/api/v2/json/repos/show/'
+
     unless params[:user].nil? or params[:repo].nil?
       result = Nestful.get(base_url+params[:user]+"/"+params[:repo])
       respond_to do |format|
