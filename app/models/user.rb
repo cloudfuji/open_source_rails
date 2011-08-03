b5bac824a9e4691ebf14bd0db565e9a090322b64
Akash Manohar J <akash@akash.im>
2011-08-04 02:58:09 +0530

    ido_id is accessible
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ActiveRecord::Base
   has_many :projects
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :username, :email, :ido_id, :password, :password_confirmation, :remember_me
 end
